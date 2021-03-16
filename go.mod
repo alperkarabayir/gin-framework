eb9f1de5b05afc96d48485bc22dc934c4f6ccec3
alperkarabayir <alperkarabayir61@gmail.com>
2021-03-16 21:50:39 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gin-framework
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/tpkeeper/gin-dump v1.0.0
+)
---
+++ server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"gin-framework/controller"
+	"gin-framework/middlewares"
+	"gin-framework/service"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+	setupLogOutput()
+	server := gin.New()
+
+	server.Static("/css", "./templates/css")
+	server.LoadHTMLGlob("templates/*.html")
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is successful"})
+			}
+		})
+	}
+
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
+
+	server.Run(":8080")
+}
